ab1b9e3298de348072aeeb803ed02c95b650993b
Narek <40229209+narekx@users.noreply.github.com>
2022-04-26 17:09:11 +0400

    Task 6 finish

--- matrix pointer hmw/6.c
+++ matrix pointer hmw/6.c
@@ -4,34 +4,50 @@
 #define N 4
 #define M 5
 
-int *max_in_row (int *arr, int *arr_end) 
+int max_in_row (int (*arr)[M], int row, int row_length)
 {
-    int *max = arr;
-    for (int *i = arr + 1; i < arr_end; i++) {
-        if (*max < *i) {
+    int max = 0;
+    for (int i = 1; i < row_length; i++) {
+        if (*(*(arr + row) + max) < *(*(arr + row) + i)) {
             max = i;
-            printf("%p  %p\n", max, i);
         }
     }
-    
+
     return max;
+}
+
+bool check_min_in_column (int (*arr)[M], int row, int column, int column_length)
+{
+    for (int i = 0; i < column_length; i++) {
+        if (*(*(arr + row) + column) >= *(*(arr + i) + column) && row != i) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 int main()
 {
     int arr[N][M] = {
-        {1, 2, 3, 3, 5},
-        {3, 4, 7, 8, 1},
-        {4, 2, 7, 6, 6},
-        {1, 3, 20, 4, 5}
+        {1, 2, 19, 11, 5},
+        {3, 9, 18, 10, 1},
+        {4, 2, 15, 14, 6},
+        {1, 3, 20, 15, 5}
     };
-    
+
+    bool find = false;
     
     for (int i = 0; i < N; i++) {
-        int *max = max_in_row(*(arr + i), *(arr + i) + M);
-        printf("%d\n", *(max + M) );
-        
-        break;
+        int column = max_in_row(arr, i, M);
+        if (find = check_min_in_column(arr, i, column, N)) {
+            printf("%d\n", *(*(arr + i) + column));
+            break;
+        }
+    }
+
+    if (!find) {
+        printf("%s\n", "NO");
     }
     
 
