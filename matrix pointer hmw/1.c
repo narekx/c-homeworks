5fe6691a9605964f6b1f80b32466b8dd12f9aebf
Narek <40229209+narekx@users.noreply.github.com>
2022-04-26 17:30:58 +0400

    Task 1 finish

--- matrix pointer hmw/1.c
+++ matrix pointer hmw/1.c
@@ -3,36 +3,62 @@
 #define N 4
 #define M 5
 
+int get_max (int (*arr)[M], int column_length, int row_length)
+{
+    int max = **arr;
+    
+    for (int i = 0; i < column_length; i++) {
+        for (int j = 0; j < row_length; j++) {
+            if (max < *(*(arr + i) + j)) {
+                max = *(*(arr + i) + j);
+            }
+        }
+    }
+
+    return max;
+}
+
+int get_row_sum (int (*arr)[M], int row, int row_length)
+{
+    int sum = 0;
+    for (int i = 0; i < row_length; i++) {
+        sum += *(*(arr + row) + i);
+    }
+
+    return sum;
+}
+
+int get_column_sum (int (*arr)[M], int column, int column_length)
+{
+    int sum = 0;
+    for (int i = 0; i < column_length; i++) {
+        sum += *(*(arr + i) + column);
+    }
+
+    return sum;
+}
+
 int main()
 {
     int arr[N][M] = {
         {1, 2, 3, 4, 5},
         {5, 4, 7, 8, 1},
         {4, 2, 7, 9, 6},
-        {1, 3, 2, 4, 5}
+        {1, 9, 2, 4, 5}
     };
-    
-    int maxi = 0;
-    int maxj = 0;
-    
+
+    int max = get_max(arr, N , M);
+
+    int sum = 0;
+
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
-            if (*(*(arr + maxi) + maxj) < *(*(arr + i) + j)) {
-                maxi = i;
-                maxj = j;
+            if (*(*(arr + i) + j) == max) {
+                sum -= max;
+                sum += get_row_sum(arr, i, M);
+                sum += get_column_sum(arr, j, N);
             }
         }
-    }
-    
-    int sum = 0 - *(*(arr + maxi) + maxj);
-    
-    for (int i = 0; i < N; i++) {
-        sum += *(*(arr + i) + maxj);
-
-    }
-    
-    for (int j = 0; j < M; j++) {
-        sum += *(*(arr + maxi) + j);
     }
     
     printf("%d\n", sum);
