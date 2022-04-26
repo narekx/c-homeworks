d1ba2a5826c300193096961fdfafa2bb236af119
Narek <40229209+narekx@users.noreply.github.com>
2022-04-26 16:13:11 +0400

    Task 9 continuous

--- matrix pointer hmw/9.c
+++ matrix pointer hmw/9.c
@@ -4,18 +4,6 @@
 #define N 4
 #define M 5
 
-// int change_values (int (*arr)[M], int first_row, int first_column)
-// {
-//     int second_row = first_row + ceil_num(N);
-//     int second_column = first_column + ceil_num(M);
-
-//     int t = *(*(arr + first_row) + first_column);
-//     *(*(arr + first_row) + first_column) = *(*(arr + second_row) + second_column);
-//     *(*(arr + second_row) + second_column) = t;
-
-//     return 0;
-// }
-
 bool check_minus (int (*arr)[M], int column, int row_length)
 {
     for (int  i = 0; i < row_length; i++) {
@@ -27,18 +15,31 @@ bool check_minus (int (*arr)[M], int column, int row_length)
     return true;
 }
 
+int change_column_values (int (*arr)[M], int first_column, int second_column, int row_length)
+{
+    for (int i = 0; i < row_length; i++) {
+        int t = *(*(arr + i) + first_column);
+        *(*(arr + i) + first_column) = *(*(arr + i) + second_column);
+        *(*(arr + i) + second_column) = t;    
+    }
+
+    return 0;
+}
+
 int delete_column (int (*arr)[M], int column, int row_lentgh, int column_length)
 {
-    for (int i = 0;)
+    for (int i = column; i < column_length - 1; i++) {
+        change_column_values(arr, i, i + 1, row_lentgh);
+    }
 }
 
 int main()
 {
     int arr[N][M] = {
-        {1, 2, -3, 4, 5},
-        {6, 7, -8, 9, 1},
-        {2, 3, -4, 5, 6},
-        {7, 8, -9, 1, 2}
+        {-1, 2, -3, 4, 5},
+        {-6, 7, -8, 9, 1},
+        {-2, 3, -4, 5, 6},
+        {-7, 8, -9, 1, 2}
     };
 
     int n = N;
@@ -46,19 +47,19 @@ int main()
     int i = 0;
     while (i < m) {
         if (check_minus(arr, i, n)) {
-            delete_column(arr, i);
+            delete_column(arr, i, n, m);
+            m--;
         }
 
         i++;
     }
     
-
-    // for (int i = 0; i < N; i++)
-    // {
-    //     for (int j = 0; j < M; j++)
-    //     {
-    //         printf("%d ", *(*(arr + i) + j));
-    //     }
-    //     printf("\n");
-    // }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            printf("%d ", *(*(arr + i) + j));
+        }
+        printf("\n");
+    }
 }
