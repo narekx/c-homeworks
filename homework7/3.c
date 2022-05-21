1a9f604c4316907db743e462a8f78c43d409d825
narekx <narek.hovsepyan01@gmail.com>
2022-05-21 14:39:12 +0400

    Task 3

---
+++ homework7/3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool is_repeat (char *str, char *s)
+{
+    for (char *i = str; *i != '\0'; i++) {
+        if (*i == *s && i != s) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int main()
+{
+    char s[100] = "qwertwyuiopetuop";
+    char news[100] = "";
+    for (char *i = s; *i != '\0'; i++) {
+        if (!is_repeat(s, i)) {
+            strncat(news, i, 1);
+        }
+    }
+    
+    printf("%s\n", news);
+
+    return 0;
+}
